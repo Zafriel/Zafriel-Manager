b75ccfc06a1a7a2651e3ac21d4b7ebdba95546d3
zSpl1nterUS_ <metalherogamer@hotmail.com>
2021-01-15 11:05:03 -0300

    updates

--- src/commands/Utils/cmd.js
+++ src/commands/Utils/cmd.js
@@ -9,10 +9,14 @@ exports.run = async (client, message, args) => {
   moment.locale("pt-BR");
   Guild.findOne({ _id: message.guild.id }, async function (err, server) {
     User.findOne({ _id: message.author.id }, async function (err, user) {
-      if (!server.cmd.find((x) => x.name == args[0].toLowerCase())) {
-        return message.quote(
-          `${message.author}, não achei nenhum comando com o nome **\`${args[0]}\`**`
-        );
+      const COMANDOS = new Discord.MessageEmbed()
+      .setColor(process.env.EMBED_COLOR)
+      .setAuthor(`${client.user.username} - Comandos`, client.user.displayAvatarURL({size: 2048}))
+      .setFooter(`Pedido por ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
+      .setThumbnail(client.user.displayAvatarURL({size: 2048}))
+      .addField(`Comandos:`, server.cmd.map((x) => `\`${x.name}\``).join(", "))
+      if (!args[0] || !server.cmd.find((x) => x.name ==  args[0].toLowerCase())) {
+        return message.quote(message.author, COMANDOS);
       } else {
         const cmd = server.cmd.find((x) => x.name == args[0].toLowerCase());
         let author = await client.users.fetch(cmd.author);
@@ -36,7 +40,10 @@ exports.run = async (client, message, args) => {
               name: "Enviado em",
               value: moment(cmd.date).format("LLL"),
             }
-          );
+          )
+          .setThumbnail(client.user.displayAvatarURL({size: 2048}))
+          .setFooter(`Pedido por ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
+
         message.channel.send(CMD);
       }
     });
